f6d14a7319d985f0a5ace27c0118f3ab4d1ad5d3
SemperFenix <dureivan@gmail.com>
2023-02-24 19:32:39 +0100

    Add app express

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import { scrubsRouter } from './router/scrubs.router.js';
 
 export const app = express();
 app.disable('x-powered-by');

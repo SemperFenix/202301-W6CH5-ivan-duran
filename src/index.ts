b4cffa89c566c604e5d7e2db0f125565d2d070ca
SemperFenix <dureivan@gmail.com>
2023-02-24 19:32:46 +0100

    Add server file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import http from 'http';
-import { app } from './app';
+import { app } from './app.js';
 
 const PORT = process.env.PORT || 4251;
 
 const server = http.createServer(app);
 
-server.on('error', () => {});
+server.on('error', () => {
+  console.log('Eres idiota');
+});
 
 server.on('listening', () => {
   console.log(

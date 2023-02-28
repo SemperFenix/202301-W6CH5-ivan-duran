f6210b2db71a7a73e821f9ad73cb3e6c3fd900f3
SemperFenix <dureivan@gmail.com>
2023-02-28 20:07:24 +0100

    Add custom error interface and class

---
+++ src/errors/errors.ts
@@ -0,0 +1,18 @@
+export interface CustomError extends Error {
+  statusCode: number;
+  statusMessage: string;
+}
+
+export class HTTPError extends Error implements CustomError {
+  constructor(
+    public statusCode: number,
+    public statusMessage: string,
+    public message: string,
+    public options?: ErrorOptions
+  ) {
+    super(message, options);
+    this.name = 'HTTP Error';
+    this.statusCode = statusCode;
+    this.statusMessage = statusMessage;
+  }
+}

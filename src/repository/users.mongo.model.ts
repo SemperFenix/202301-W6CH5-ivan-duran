b7d537ce82aeb4aac9dd266e0c7d4464523145db
SemperFenix <dureivan@gmail.com>
2023-03-01 19:20:47 +0100

    Add users mongoose model

---
+++ src/repository/users.mongo.model.ts
@@ -0,0 +1,25 @@
+import { model, Schema } from 'mongoose';
+import { User } from '../entities/user.model.js';
+
+const userSchema = new Schema<User>({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.set('toJSON', {
+  transform(_document, returnedObject) {
+    returnedObject.id = returnedObject._id;
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.password;
+  },
+});
+
+export const UserModel = model('User', userSchema, 'users');

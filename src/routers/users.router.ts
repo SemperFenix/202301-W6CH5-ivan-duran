8bdb02ab01e1134d020e5fe1a278e425f8b401bd
SemperFenix <dureivan@gmail.com>
2023-03-01 19:22:03 +0100

    Add users router

---
+++ src/routers/users.router.ts
@@ -0,0 +1,10 @@
+import { Router as router } from 'express';
+import { UsersController } from '../controllers/users.controller.js';
+import { UsersMongoRepo } from '../repository/users.mongo.repo.js';
+
+export const usersRouter = router();
+export const repo = new UsersMongoRepo();
+const controller = new UsersController(repo);
+
+usersRouter.post('/register', controller.register.bind(controller));
+usersRouter.post('/login', controller.login.bind(controller));

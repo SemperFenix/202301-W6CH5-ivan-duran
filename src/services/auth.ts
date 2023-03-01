a0f2fc0fae9c7600df30442f6d2332c80d64b8e3
SemperFenix <dureivan@gmail.com>
2023-03-01 19:17:53 +0100

    Add auth class and static methods

---
+++ src/services/auth.ts
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken';
+import { config } from '../config.js';
+import bcrypt from 'bcryptjs';
+
+type TokenPayload = {
+  email: string;
+  role: string;
+};
+
+const salt = 10;
+
+export class Auth {
+  static signJWT(payload: TokenPayload) {
+    if (!config.secret) return;
+    return jwt.sign(payload, config.secret);
+  }
+
+  static verifyJWT(token: string) {
+    if (!config.secret) return;
+    const verify = jwt.verify(token, config.secret);
+    if (typeof verify === 'string') throw new Error('Invalid token');
+    return verify;
+  }
+
+  static hash(value: string) {
+    return bcrypt.hash(value, salt);
+  }
+
+  static compare(value: string, hash: string) {
+    return bcrypt.compare(value, hash);
+  }
+}
